4dabe9b3def619f7870594a0322490da9fd2fcd5
kostia <i.const.yank@gmail.com>
2019-10-26 12:05:07 +0300

    start

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,51 @@
 const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const fs = require('fs');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const LiveReloadPlugin = require('webpack-livereload-plugin');
+
+const PAGES_DIR = `${path.join(__dirname, 'src')}/pug/`;
+const PAGES = fs.readdirSync(PAGES_DIR).filter(fileName => fileName.endsWith('.pug'));
+
 module.exports = {
     entry: './src/app.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        publicPath: ''
     },
     module: {
         rules: [
             {
-                test: /\.scss$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: ['css-loader', 'sass-loader']
-                })
+                test: /\.(scss|css)$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.pug$/,
+                loader: 'pug-loader'
             }
-        ]
+        ],
     },
     plugins: [
-        new ExtractTextPlugin('style.css')
-    ]
+        new MiniCssExtractPlugin({
+            filename: 'style.css',
+        }),
+        // new LiveReloadPlugin(options),
+        ...PAGES.map(page => new HtmlWebpackPlugin({
+            template: `${PAGES_DIR}/${page}`,
+            filename: `./${page.replace(/\.pug/,'.html')}`
+        })),
+    ],
+    watchOptions: {
+        poll: true
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000
+    }
 };
