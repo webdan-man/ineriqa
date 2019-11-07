48a293cd6a68a5a386ac6476087f3ae86ef35cf9
kostia <i.const.yank@gmail.com>
2019-11-07 12:08:18 +0200

    update

--- webpack.config.js
+++ webpack.config.js
@@ -47,7 +47,7 @@ module.exports = {
                   loader: 'url-loader',
                   options: { 
                       limit: 8000, // Convert images < 8kb to base64 strings
-                      name: 'img/[hash]-[name].[ext]'
+                      name: 'img/[hash].[ext]'
                   } 
               }]
           },
